fb4b2ee5128dec149d8a06275d6f5a3d26b85a37
lochanaEdirisinghe <edirisinghelochana@gmail.com>
2020-02-14 20:03:46 +0530

    add profile

--- src/chatApp.java
+++ src/chatApp.java
@@ -1,11 +1,15 @@
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +17,9 @@ import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/chat")
 public class chatApp extends HttpServlet {
+    String userid="";
+    String username="";
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BasicDataSource db = (BasicDataSource) getServletContext().getAttribute("db");
@@ -31,22 +38,66 @@ public class chatApp extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BasicDataSource db = (BasicDataSource) getServletContext().getAttribute("db");
-        try (Connection connection = db.getConnection()) {
-            String name=req.getParameter("name");
-            String password = req.getParameter("password");
-            PreparedStatement pst = connection.prepareStatement("select * from user where name=? and password=?");
-            pst.setObject(1, name);
-            pst.setObject(2, password);
-            ResultSet rst = pst.executeQuery();
-            if(rst.next()){
-                resp.getWriter().print("ok");
-            }else{
-                resp.getWriter().print("wrong username and password");
-            }
+        String option = req.getParameter("option");
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        BasicDataSource db = (BasicDataSource) getServletContext().getAttribute("db");
+        switch (option) {
+            case  "getAll":
+
+            try (Connection connection = db.getConnection()) {
+                PreparedStatement pstm = connection.prepareStatement("Select * from user");
+                ResultSet rst = pstm.executeQuery();
+                System.out.println(rst);
+                JsonArrayBuilder customerArray = Json.createArrayBuilder();
+                JsonArrayBuilder customerArray2 = Json.createArrayBuilder();
+
+                while (rst.next()) {
+                    JsonObjectBuilder customer = Json.createObjectBuilder();
+                    String id = rst.getString(1);
+                    String name = rst.getString(2);
+                    if(!id.equals(userid)) {
+                        customer.add("id", id);
+                        customer.add("name", name);
+                        customerArray.add(customer.build());
+                    }
+
+                }
+
+                PrintWriter writer = resp.getWriter();
+                writer.print(customerArray.build());
+                /*JsonObjectBuilder customer2 = Json.createObjectBuilder();
+                customer2.add("id", userid);
+                customer2.add("name", username);
+                customerArray2.add(customer2.build());*/
+                //writer.print(customerArray2.build());
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            break;
+
+            case  "getUser":
+
+            try (Connection connection = db.getConnection()) {
+                String name = req.getParameter("name");
+                String password = req.getParameter("password");
+                PreparedStatement pst = connection.prepareStatement("select * from user where name=? and password=?");
+                pst.setObject(1, name);
+                pst.setObject(2, password);
+                ResultSet rst = pst.executeQuery();
+                if (rst.next()) {
+                    userid=rst.getString(1);
+                    username=rst.getString(2);
+                    resp.getWriter().print("ok");
+                } else {
+                    resp.getWriter().print("wrong username and password");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            break;
         }
     }
 
